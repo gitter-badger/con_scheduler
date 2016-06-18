0571e26e7223ca58c16b7b5ff10cfd80247aa1ba
Brian Bommarito <me@brianbommarito.com>
2016-06-18 14:07:28 -0400

    Adding a basic User model.
    Signed-off-by: Brian Bommarito <me@brianbommarito.com>

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  it { should have_db_column(:created_at).
+      of_type(:datetime) }
+
+  it { should have_db_column(:updated_at).
+      of_type(:datetime) }
+
+  it { should have_db_column(:email).
+      of_type(:string).
+      with_options(null: false) }
+
+  it { should have_db_index(:email).unique(true) }
+end
